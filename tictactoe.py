71fe4a790e0e5f12582f143d5c01003db943cc71
archanasri <archanasrisubramanian@gmail.com>
2023-07-30 13:24:28 -0700

    Creating user interface and player input

--- tictactoe.py
+++ tictactoe.py
@@ -8,3 +8,22 @@ class TicTacToe:
 			for j in range(3):
 				row.append('-')
 				self.board.append(row)
+
+	def get_move(player):
+		while True:
+			try:
+				row = int(input(f"Player {player}, enter row number (0, 1, or 2): "))
+				col = int(input(f"Player {player}, enter column number (0, 1, or 2): "))
+				if 0 <= row < 3 and 0 <= col < 3:
+					return row, col
+				else:
+					print("Invalid input! Row and column should be between 0 and 2.")
+			except ValueError:
+				print("Invalid input! Please enter numbers for row and column.")
+
+	def make_move(board, player, row, col):
+		if board[row][col] == ' ':
+			board[row][col] = player
+			return True
+
+		return False
