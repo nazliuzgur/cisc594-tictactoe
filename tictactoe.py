74ecd492905255de45e53d8d7ecc416e311f81f8
archanasri <archanasrisubramanian@gmail.com>
2023-07-31 12:40:00 -0700

    Adding game logic

--- tictactoe.py
+++ tictactoe.py
@@ -27,3 +27,34 @@ class TicTacToe:
 			return True
 
 		return False
+
+	def check_win(board, player):
+		for row in board:
+			if all(cell == player for cell in row):
+				return True
+
+		for col in range(3):
+			if all(board[row][col] == player for row in range(3)):
+				return True
+
+		if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+			return True
+
+		return False
+
+	def check_tie(board):
+		for row in board:
+			if ' ' in row:
+				return False
+		return True
+
+	def is_game_over(board, player):
+		return self.check_win(board, player) or self.check_tie(board)
+
+	def get_winner(board, player1, player2):
+		if check_win(board, player1):
+			return player1
+		elif check_win(board, player2):
+			return player2
+		else:
+			return None
