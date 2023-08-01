dec27d9837ed437d62f6a5571e2c094a8ef15927
archanasri <archanasrisubramanian@gmail.com>
2023-08-01 13:02:19 -0700

    Adding error handling and retry mechanism

--- tictactoe.py
+++ tictactoe.py
@@ -58,3 +58,44 @@ class TicTacToe:
 			return player2
 		else:
 			return None
+
+	def play_again():
+		response = input("Do you want to play again? (yes/no): ")
+		return response.lower() == 'yes'
+
+	def reset_board(board):
+		for i in range(3):
+			for j in range(3):
+				board[i][j] = ' '
+
+	def tic_tac_toe_game():
+		player1 = 'X'
+		player2 = 'O'
+
+		while True:
+			board = create_board()
+			current_player = player1
+
+			while not is_game_over(board, current_player):
+				display_board(board)
+				row, col = get_move(current_player)
+
+				if not make_move(board, current_player, row, col):
+					print("Cell already taken. Try again.")
+					continue
+
+				current_player = player2 if current_player == player1 else player1
+
+		display_board(board)
+
+		winner = get_winner(board, player1, player2)
+		if winner:
+			print(f"Player {winner} wins!")
+		else:
+			print("It's a tie!")
+
+		if not play_again():
+			break
+
+if __name__ == "__main__":
+    tic_tac_toe_game()
