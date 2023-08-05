3d63c7abe6f91d763a94ed81cd5e92666e19d860
archanasri <archanasrisubramanian@gmail.com>
2023-08-05 15:24:16 -0400

    Developing AI opponent

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
+import math
+
 class TicTacToe:
 	def create_board(self):
 		return [[' ' for _ in range(3)] for _ in range(3)]
@@ -94,6 +96,60 @@ class TicTacToe:
 
 			if not self.play_again():
 				break
+	
+	def evaluate(self, board, player1, player2):
+		if self.check_win(board, player1):
+			return 1
+		elif self.check_win(board, player2):
+			return -1
+		else:
+			return 0
+
+	def minimax(self, board, depth, is_maximizing, player1, player2):
+		if self.check_win(board, player1):
+			return 1
+		elif self.check_win(board, player2):
+			return -1
+		elif self.check_tie(board):
+			return 0
+
+		if is_maximizing:
+			best_score = -math.inf
+			for i in range(3):
+				for j in range(3):
+					if board[i][j] == ' ':
+						board[i][j] = player1
+						score = self.minimax(board, depth + 1, False, player1, player2)
+						board[i][j] = ' '
+						best_score = max(score, best_score)
+			return best_score
+		else:
+			best_score = math.inf
+			for i in range(3):
+				for j in range(3):
+					if board[i][j] == ' ':
+						board[i][j] = player2
+						score = self.minimax(board, depth + 1, True, player1, player2)
+						board[i][j] = ' '
+						best_score = min(score, best_score)
+			return best_score
+
+	def get_best_move(self, board, player1, player2):
+		best_score = -math.inf
+		best_move = None
+
+		for i in range(3):
+			for j in range(3):
+				if board[i][j] == ' ':
+					board[i][j] = player1
+					score = self.minimax(board, 0, False, player1, player2)
+					board[i][j] = ' '
+
+					if score > best_score:
+						best_score = score
+						best_move = (i, j)
+
+		return best_move
 
 if __name__ == "__main__":
 	game = TicTacToe()
